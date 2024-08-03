d6944e84cfd0f20c49bec634c8a6a7beb9f47cdf
reinexxism <lucy0805@naver.com>
2024-08-03 21:43:02 +0900

    Develop : HomeView 1차 페이지 구현 완료

--- likelion_hackathon/HomeView.swift
+++ likelion_hackathon/HomeView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("이곳은 서비스의 타이틀이 들어갈 곳\n아직 미정입니다.")
+                    .padding(.top, 300)
+                
+                Spacer()
+                
+                NavigationLink(destination: LoginView()) {
+                    Text("나의 운동 기록 시작하기")
+                        .padding(.horizontal, 70)
+                        .padding(.vertical, 25)
+                        .background(.black)
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                        
+                }
+                .padding(.bottom, 150)
+            }
+        }
     }
 }
 
--- likelion_hackathon/likelion_hackathonApp.swift
+++ likelion_hackathon/likelion_hackathonApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct likelion_hackathonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
