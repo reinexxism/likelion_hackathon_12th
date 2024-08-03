e99ec06696a9c6c6a9498f03157d6d3cdab88754
reinexxism <lucy0805@naver.com>
2024-08-03 18:46:56 +0900

    Add : 페이지별 뷰를 추가했습니다.

---
+++ likelion_hackathon/UserSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettingView.swift
+//  likelion_hackathon
+//
+//  Created by 최주현 on 8/3/24.
+//
+
+import SwiftUI
+
+struct UserSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSettingView()
+}
