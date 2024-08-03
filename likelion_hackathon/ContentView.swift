896a67fd57fba2db662957f60ca2abe84d43435d
reinexxism <lucy0805@naver.com>
2024-08-03 18:35:42 +0900

    Initial Commit

---
+++ likelion_hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  likelion_hackathon
+//
+//  Created by 최주현 on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ likelion_hackathon/likelion_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  likelion_hackathonApp.swift
+//  likelion_hackathon
+//
+//  Created by 최주현 on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct likelion_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
